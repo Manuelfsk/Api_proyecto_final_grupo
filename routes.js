e0eb0ff703e921986dccc7ead5ba6cffa2c4671f
Manuel Fonseca Avila <fonsecafernando88@gmail.com>
2024-05-27 09:26:15 -0500

    creacion de las rutas para Usuarios

--- routes.js
+++ routes.js
@@ -1,22 +1,48 @@
+
+//crud Tags 
 let tagsController = require("./API/controladores/tagsController.js").tagsController
 
-
+//create == crear elemento
 app.post("/tags/create", function (request, response) {
     tagsController.create(request, response)
 })
-
+// read == listar todos los elementos
 app.get("/tags/read", function (request, response) {
     tagsController.read(request, response)
 })
-
+// readId == lista un solo elemento por ID
 app.get("/tags/readId", function (request, response) {
     tagsController.readId(request, response)
 })
-
+// update == modificar elementos 
 app.put("/tags/update", function (request, response) {
     tagsController.update(request, response)
 })
-
+// delete  == eliminar elementos
 app.delete("/tags/delete", function (request, response) {
     tagsController.delete(request, response)
+})
+
+//crud usuarios 
+let usuariosController = require("./API/controladores/usuariosController.js").usuariosController
+
+//create == crear elemento
+app.post("/usuarios/create", function (request, response) {
+    usuariosController.create(request, response)
+})
+// read == listar todos los elementos
+app.get("/usuarios/read", function (request, response) {
+    usuariosController.read(request, response)
+})
+// readId == lista un solo elemento por ID
+app.get("/usuarios/readId", function (request, response) {
+    usuariosController.readId(request, response)
+})
+// update == modificar elementos 
+app.put("/usuarios/update", function (request, response) {
+    usuariosController.update(request, response)
+})
+// delete  == eliminar elementos
+app.delete("/usuarios/delete", function (request, response) {
+    usuariosController.delete(request, response)
 })
