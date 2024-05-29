e34da40aa90b7c078bc991ee7781b7c16e673953
Manuel Fonseca Avila <fonsecafernando88@gmail.com>
2024-05-29 18:06:52 -0500

    arreglo del http://localhost y variables email y password

--- pagina/index.js
+++ pagina/index.js
@@ -1,6 +1,23 @@
-let login= function(){
+let login = function () {
     let email = document.getElementById("email").value
     let password = document.getElementById("password").value
     console.log(email)
     console.log(password)
+    // WARNING: For POST requests, body is set to null by browsers.
+    var data = "email=" + email + "&password=" + password;
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("POST", "http://localhost:3001/usuarios/login");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.send(data);
+
 }
