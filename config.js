99d5e9d12efb6d72ba627a2d4ba000aab5fa9002
Manuel Fonseca Avila <fonsecafernando88@gmail.com>
2024-06-25 15:01:04 -0500

    ajuste cors

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ let config = {}
 config.puerto = 3000
 config.bd = "proyectoFbit"
 config.origin = [
-    "http://localhost:4200",
-    "undefined"
+    "http://localhost:4200"
 ]
 module.exports.config = config
