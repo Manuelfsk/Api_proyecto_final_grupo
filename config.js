5d6e3f66fafd53bb042b6cd13eb6837f1024ec81
Manuel Fonseca Avila <fonsecafernando88@gmail.com>
2024-06-26 08:44:41 -0500

    cracion de codigo y correo de activacion

--- config.js
+++ config.js
@@ -5,4 +5,6 @@ config.bd = "proyectoFbit"
 config.origin = [
     "http://localhost:4200"
 ]
+config.passgmail = "eahk byxl knyr vylf"
+config.usergmail = "validaciondecorreose@gmail.com"
 module.exports.config = config
